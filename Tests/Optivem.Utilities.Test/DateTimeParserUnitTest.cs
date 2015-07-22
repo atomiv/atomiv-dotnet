17f946d20005c3a1c90a364ed60f107c54ed810c
Valentina Cupać <vcupac@users.noreply.github.com>
2015-07-22 21:19:01 +0200

    Updated project properties

--- Tests/Optivem.Utilities.Test/DateTimeParserUnitTest.cs
+++ Tests/Optivem.Utilities.Test/DateTimeParserUnitTest.cs
@@ -6,9 +6,25 @@ namespace Optivem.Utilities.Test
     [TestClass]
     public class DateTimeParserUnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Initialize()
         {
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestParseDateTime1()
+        {
+            DateTimeParser _parser = new DateTimeParser("yyyy-MM-dd");
+            string dateTimeString = "1996-04-01";
+            DateTime actual = _parser.ParseDateTime(dateTimeString);
+            DateTime expected = new DateTime(1996, 4, 1);
+            Assert.AreEqual(actual, expected);
+
         }
     }
 }
