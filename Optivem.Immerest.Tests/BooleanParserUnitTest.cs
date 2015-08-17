0f8b7256c840b83aba95a8112b68de4f7aeee641
Valentina Cupać <valentina.cupac@optivem.com>
2015-08-17 22:21:22 +0200

    Updated constructor for boolean parser to use dictionary

--- Optivem.Immerest.Tests/BooleanParserUnitTest.cs
+++ Optivem.Immerest.Tests/BooleanParserUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Optivem.Immerest.Test
 {
@@ -13,6 +14,25 @@ namespace Optivem.Immerest.Test
 
             bool actual = parser.ParseBoolean("true");
             bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestParseBoolean2()
+        {
+            Dictionary<string, bool> mapping = new Dictionary<string, bool>
+            {
+                { "Yes", true },
+                { "No", false },
+                { "Y", true },
+                { "N", false }
+            };
+
+            BooleanParser parser = new BooleanParser(mapping);
+
+            bool actual = parser.ParseBoolean("No");
+            bool expected = false;
 
             Assert.AreEqual(expected, actual);
         }
