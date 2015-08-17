f92285e1077df4eaafa4bb5ccca2f899dcb33bdc
Valentina Cupać <valentina.cupac@optivem.com>
2015-08-17 21:26:55 +0200

    Added comments

--- Optivem.Immerest/DateTimeParser.cs
+++ Optivem.Immerest/DateTimeParser.cs
@@ -12,13 +12,25 @@ namespace Optivem.Immerest
     /// </summary>
     public class DateTimeParser
     {
+        /// <summary>
+        /// Constructs an instance of the parser
+        /// </summary>
+        /// <param name="dateTimeFormat">Datetime format used for parsing dates</param>
         public DateTimeParser(string dateTimeFormat)
         {
             this.DateTimeFormat = dateTimeFormat;
         }
 
+        /// <summary>
+        /// Datetime format used for parsing dates
+        /// </summary>
         public string DateTimeFormat { get; private set; }
 
+        /// <summary>
+        /// Converts string representation of DateTime to the actual DateTime object
+        /// </summary>
+        /// <param name="data">String data to be converted</param>
+        /// <returns>Converted DateTime</returns>
         public DateTime ParseDateTime(string data)
         {
             CultureInfo provider = CultureInfo.InvariantCulture;
