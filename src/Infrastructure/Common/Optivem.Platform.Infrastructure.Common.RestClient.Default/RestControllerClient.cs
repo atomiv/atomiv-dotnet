4f5971afcd98d3ec11346ebbe61068cbadd0caa2
Valentina Cupac <valentina.cupac@optivem.com>
2019-01-30 20:37:07 +0100

    #75 Rest Client - Created RestControllerClient

---
+++ src/Infrastructure/Common/Optivem.Platform.Infrastructure.Common.RestClient.Default/RestControllerClient.cs
@@ -0,0 +1,12 @@
+﻿using Optivem.Platform.Core.Common.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optivem.Platform.Infrastructure.Common.RestClient.Default
+{
+    public class RestControllerClient : IRestControllerClient
+    {
+
+    }
+}
