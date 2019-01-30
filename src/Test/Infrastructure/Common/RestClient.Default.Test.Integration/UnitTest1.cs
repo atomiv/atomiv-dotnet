df3623e1ac5d2dab09283a10735d37ba6327456f
Valentina Cupac <valentina.cupac@optivem.com>
2019-01-30 21:06:43 +0100

    #75 Rest Client - Created Test project

---
+++ src/Test/Infrastructure/Common/RestClient.Default.Test.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Optivem.Platform.Infrastructure.Core.RestClient.Default.Test.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
