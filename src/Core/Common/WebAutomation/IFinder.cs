d0c2f979812a954f2807ffcbfff9ef7f45f21712
Valentina Cupac <valentina.cupac@optivem.com>
2019-03-20 20:34:14 +0100

    #178 Selenium - Setup

---
+++ src/Core/Common/WebAutomation/IFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optivem.Platform.Core.Common.WebAutomation
+{
+    public interface IFinder
+    {
+        ITextBox FindTextBoxByName(string name);
+    }
+}
---
+++ src/Infrastructure/Common/WebAutomation.Selenium/SeleniumTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Optivem.Platform.Infrastructure.Common.WebAutomation.Selenium
+{
+    public class SeleniumTextBox : ITextBox
+    {
+        private IWebElement _element;
+
+        public SeleniumTextBox(IWebElement element)
+        {
+            _element = element;
+        }
+
+        public string GetText()
+        {
+            return _element.GetAttribute("value");
+        }
+
+        public void SetText(string text)
+        {
+            _element.Clear();
+            _element.SendKeys(text);
+        }
+
+        // TODO: VC: Protected --> IWebElement or public?
+
+        /*
+         * 
+            var element = driver.FindElement(By.Name("firstname"));
+            element.Clear();
+            element.SendKeys("This is my name");
+
+
+            var value = element.GetAttribute("value");
+         * 
+         */
+    }
+}
