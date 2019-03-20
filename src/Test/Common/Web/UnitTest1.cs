7c7a08e720d247c27cb61fbf6380b1a5f4631903
Valentina Cupac <valentina.cupac@optivem.com>
2019-03-20 20:24:40 +0100

    #178 Selenium - Setup

---
+++ src/Test/Common/Web/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Optivem.Platform.Test.Common.Web
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
