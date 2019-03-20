f4f527d8d01397e73158f4a87c6c5712ec758078
Valentina Cupac <valentina.cupac@optivem.com>
2019-03-20 20:49:46 +0100

    #178 Selenium - Setup

---
+++ src/Test/Infrastructure/Common/WebAutomation.Selenium/SeleniumFinderTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using Optivem.Platform.Infrastructure.Common.WebAutomation.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Optivem.Platform.Test.Infrastructure.Common.WebAutomation.Selenium
+{
+    public class SeleniumFinderTest
+    {
+        [Fact]
+        public void TestFindTextBoxByName()
+        {
+            var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            using (var driver = new ChromeDriver(currentDir))
+            {
+                driver.Url = "https://www.toolsqa.com/automation-practice-form/";
+
+                var finder = new SeleniumFinder(driver);
+
+                var textbox = finder.FindTextBoxByName("firstname");
+
+                textbox.SetText("This is my name");
+                var text = textbox.GetText();
+            }
+        }
+    }
+}
---
+++ src/Test/Infrastructure/Common/WebAutomation.Selenium/SeleniumTextBoxTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Optivem.Platform.Test.Infrastructure.Common.WebAutomation.Selenium
+{
+    public class SeleniumTextBoxTest
+    {
+        [Fact]
+        public void TestSetGetText()
+        {
+
+        }
+    }
+}
