1844d65ce8c3da94c7154465a31757630bbb43f1
Valentina Cupać <valentina.cupac@optivem.com>
2019-04-04 19:45:47 +0200

    #186 Test - Common

---
+++ src/Test/Common/Selenium/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Optivem.Platform.Test.Common.Xunit.Selenium
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
