b66271ccc40f9878744bbd3e6509b9bee1ee242d
Valentina Cupać <valentina.cupac@optivem.com>
2019-05-30 20:20:09 +0200

    #59 Custom Exceptions

---
+++ src/Core/Application.Interface/Exceptions/ApplicationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Optivem.Core.Application
+{
+    public class ApplicationException : Exception
+    {
+        public ApplicationException()
+        {
+        }
+
+        public ApplicationException(string message) : base(message)
+        {
+        }
+
+        public ApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ApplicationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Core/Domain/Exceptions/DomainException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Optivem.Core.Domain
+{
+    public class DomainException : Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DomainException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
