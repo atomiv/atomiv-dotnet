655a2a0f302f0032175a775312e3829dab36c233
Valentina Cupać <valentina.cupac@optivem.com>
2019-08-20 22:46:44 +0200

    #270 AppVeyor - SSL Certificate

--- configure.ps1
+++ configure.ps1
@@ -47,6 +47,21 @@ $securePassword = ConvertTo-SecureString $password -asplaintext -force
 
 Import-PfxCertificate -FilePath $certPath -CertStoreLocation Cert:\LocalMachine\My -Password $securePassword
 
+
+function Install-Certificate ($certificatePath, [string]$storeLocation = "LocalMachine", [string]$storeName = "My")
+{
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificatePath, "", "MachineKeySet,PersistKeySet")
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeName, $storeLocation)
+    $store.Open("ReadWrite")
+    $store.Add($cert)
+    $store.Close()
+    "Thumbprint: $($cert.Thumbprint)"
+}
+
+Install-Certificate $certPath
+
+
+
 # TODO: VC: DELETE
 # Write-Host "Trying to run web api"
 # dotnet run --project .\template\src\Web\RestApi\Optivem.Template.Web.RestApi.csproj --environment Staging
