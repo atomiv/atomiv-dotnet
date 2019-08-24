60426a2c92aa42e4afeccfb48a43092622fff3b4
Valentina Cupać <valentina.cupac@optivem.com>
2019-08-24 08:49:51 +0200

    #270 AppVeyor - SSL Certificate

--- install-cert4.ps1
+++ install-cert4.ps1
@@ -10,12 +10,12 @@ $certificate = New-SelfSignedCertificate `
     -KeyLength 2048 `
     -NotBefore (Get-Date) `
     -NotAfter (Get-Date).AddYears(2) `
-    -CertStoreLocation "cert:CurrentUser\My" `
-    -FriendlyName "Localhost Certificate for .NET Core" `
+    -CertStoreLocation "cert:LocalMachine\My" `
+    -FriendlyName "Localhost2 Certificate for .NET Core" `
     -HashAlgorithm SHA256 `
     -KeyUsage DigitalSignature, KeyEncipherment, DataEncipherment `
     -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") 
-$certificatePath = 'Cert:\CurrentUser\My\' + ($certificate.ThumbPrint)  
+$certificatePath = 'Cert:\LocalMachine\My\' + ($certificate.ThumbPrint)  
 
 # create temporary certificate path
 # $tmpPath = "C:\tmp"
@@ -28,10 +28,10 @@ New-Item -ItemType Directory -Force -Path $tmpPath
 
 # set certificate password here
 $pfxPassword = ConvertTo-SecureString -String "YourSecurePassword" -Force -AsPlainText
-$pfxFilePath = "C:\projects\framework-dotnetcore\template\src\Web\RestApi\localhost.pfx"
-$cerFilePath = "C:\projects\framework-dotnetcore\template\src\Web\RestApi\localhost.cer"
-#$pfxFilePath = "D:\GitHub\optivem\framework-dotnetcore\template\src\Web\RestApi\localhost.pfx"
-#$cerFilePath = "D:\GitHub\optivem\framework-dotnetcore\template\src\Web\RestApi\localhost.cer"
+$pfxFilePath = "C:\projects\framework-dotnetcore\template\src\Web\RestApi\localhost2.pfx"
+$cerFilePath = "C:\projects\framework-dotnetcore\template\src\Web\RestApi\localhost2.cer"
+#$pfxFilePath = "D:\GitHub\optivem\framework-dotnetcore\template\src\Web\RestApi\localhost2.pfx"
+#$cerFilePath = "D:\GitHub\optivem\framework-dotnetcore\template\src\Web\RestApi\localhost2.cer"
 
 Write-Host "Exporting..."
 
@@ -46,7 +46,19 @@ Import-PfxCertificate -FilePath $pfxFilePath Cert:\LocalMachine\My -Password $pf
 
 # trust the certificate by importing the pfx certificate into your trusted root
 # TODO: VC: Check if needed
-Import-Certificate -FilePath $cerFilePath -CertStoreLocation Cert:\CurrentUser\Root
+# Import-Certificate -FilePath $cerFilePath -CertStoreLocation Cert:\CurrentUser\Root
+
+
+
+# https://social.technet.microsoft.com/Forums/ie/en-US/aac8cfcd-6bd2-423f-895b-a6612459eb16/importcertificate-without-confirmation-ignore-security-warnings?forum=winserverpowershell
+
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($cerFilePath)
+$rootStore = Get-Item Cert:\LocalMachine\Root
+$rootStore.Open("ReadWrite")
+$rootStore.Add($cert)
+$rootStore.Close()
+
+
 
 # optionally delete the physical certificates (don’t delete the pfx file as you need to copy this to your app directory)
 # Remove-Item $pfxFilePath
