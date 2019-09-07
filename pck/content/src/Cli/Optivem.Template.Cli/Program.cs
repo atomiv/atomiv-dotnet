b299f4cdc0f1e4979603a85c4eaaabd56a7ad474
Valentina Cupać <valentina.cupac@optivem.com>
2019-09-07 21:02:11 +0200

    Published Template 1.0.16.1

---
+++ pck/content/src/Cli/Optivem.Template.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Optivem.Template.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
