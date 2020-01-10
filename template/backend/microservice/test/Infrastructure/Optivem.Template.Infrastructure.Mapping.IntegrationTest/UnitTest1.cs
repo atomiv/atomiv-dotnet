f95d0690bde79f14658df772f57f73e3f480949d
Valentina Cupać <valentina.cupac@optivem.com>
2020-01-10 15:12:58 +0100

    #477 Template - Infrastructure - Integration Tests

---
+++ template/backend/microservice/test/Infrastructure/Optivem.Template.Infrastructure.Mapping.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Optivem.Template.Infrastructure.Mapping.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
