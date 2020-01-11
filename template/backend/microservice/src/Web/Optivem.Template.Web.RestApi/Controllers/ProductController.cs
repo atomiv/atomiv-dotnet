c37cae59726ea5c2dfe09672cbd1050204f4ce71
Valentina Cupać <valentina.cupac@optivem.com>
2020-01-11 15:44:29 +0100

    #476 Template - Web - Integration Tests

--- template/backend/microservice/src/Web/Optivem.Template.Web.RestApi/Controllers/ProductController.cs
+++ template/backend/microservice/src/Web/Optivem.Template.Web.RestApi/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Optivem.Framework.Core.Application;
+using Optivem.Template.Core.Application.Products.Commands;
 using Optivem.Template.Core.Application.Products.Queries;
+using System;
 using System.Threading.Tasks;
 
 namespace Optivem.Template.Web.RestApi.Controllers
@@ -16,6 +18,54 @@ namespace Optivem.Template.Web.RestApi.Controllers
             _messageBus = messageBus;
         }
 
+        #region Commands
+
+        [HttpPost(Name = "create-product")]
+        [ProducesResponseType(typeof(CreateProductCommandResponse), 201)]
+        public async Task<ActionResult<CreateProductCommandResponse>> CreateProductAsync(CreateProductCommand request)
+        {
+            var response = await _messageBus.SendAsync(request);
+            return CreatedAtRoute("find-product", new { id = response.Id }, response);
+        }
+
+        [HttpPost("{id}/relist", Name = "relist-product")]
+        [ProducesResponseType(typeof(RelistProductCommandResponse), 200)]
+        public async Task<ActionResult<RelistProductCommandResponse>> RelistProductAsync(Guid id)
+        {
+            var request = new RelistProductCommand
+            {
+                Id = id,
+            };
+
+            var response = await _messageBus.SendAsync(request);
+            return Ok(response);
+        }
+
+        [HttpPost("{id}/unlist", Name = "unlist-product")]
+        [ProducesResponseType(typeof(UnlistProductCommandResponse), 200)]
+        public async Task<ActionResult<UnlistProductCommandResponse>> UnlistProductAsync(Guid id)
+        {
+            var request = new UnlistProductCommand
+            {
+                Id = id,
+            };
+
+            var response = await _messageBus.SendAsync(request);
+            return Ok(response);
+        }
+
+        [HttpPut(Name = "update-product")]
+        [ProducesResponseType(typeof(UpdateProductCommandResponse), 200)]
+        public async Task<ActionResult<UpdateProductCommandResponse>> UpdateProductAsync(UpdateProductCommand request)
+        {
+            var response = await _messageBus.SendAsync(request);
+            return Ok(response);
+        }
+
+        #endregion
+
+        #region Queries
+
         [HttpGet(Name = "browse-products")]
         [ProducesResponseType(typeof(BrowseProductsQueryResponse), 200)]
         public async Task<ActionResult<BrowseProductsQueryResponse>> BrowseProductsAsync([FromQuery] int? page = null, [FromQuery] int? size = null)
@@ -30,6 +80,20 @@ namespace Optivem.Template.Web.RestApi.Controllers
             return Ok(response);
         }
 
+        [HttpGet("{id}", Name = "find-product")]
+        [ProducesResponseType(typeof(FindProductQueryResponse), 200)]
+        public async Task<ActionResult<FindProductQueryResponse>> FindProductAsync(Guid id)
+        {
+            var request = new FindProductQuery
+            {
+                Id = id,
+            };
+
+            var response = await _messageBus.SendAsync(request);
+            return Ok(response);
+        }
+
+
         [HttpGet("list", Name = "list-products")]
         [ProducesResponseType(typeof(ListProductsQueryResponse), 200)]
         public async Task<ActionResult<ListProductsQueryResponse>> ListProductsAsync()
@@ -38,5 +102,11 @@ namespace Optivem.Template.Web.RestApi.Controllers
             var response = await _messageBus.SendAsync(request);
             return Ok(response);
         }
+
+        #endregion
+
+
+
+
     }
 }
--- template/backend/microservice/test/Web/Optivem.Template.Web.RestApi.IntegrationTest/Fixtures/Test.cs
+++ template/backend/microservice/test/Web/Optivem.Template.Web.RestApi.IntegrationTest/Fixtures/Test.cs
@@ -1,9 +1,12 @@
-﻿using Optivem.Framework.Core.Common.Http;
+﻿using FluentAssertions;
+using Optivem.Framework.Core.Common.Http;
 using Optivem.Framework.Test.Xunit;
 using Optivem.Template.Core.Application.Customers.Commands;
+using Optivem.Template.Core.Application.Orders.Commands;
 using Optivem.Template.Core.Application.Products.Commands;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Optivem.Template.Web.RestApi.IntegrationTest.Fixtures
@@ -38,6 +41,19 @@ namespace Optivem.Template.Web.RestApi.IntegrationTest.Fixtures
             return createResponses;
         }
 
+        protected async Task<List<IObjectClientResponse<CreateOrderCommandResponse>>> CreateOrdersAsync(IEnumerable<CreateOrderCommand> createRequests)
+        {
+            var createResponses = new List<IObjectClientResponse<CreateOrderCommandResponse>>();
+
+            foreach (var createRequest in createRequests)
+            {
+                var createResponse = await CreateOrderAsync(createRequest);
+                createResponses.Add(createResponse);
+            }
+
+            return createResponses;
+        }
+
         protected async Task<List<IObjectClientResponse<CreateProductCommandResponse>>> CreateProductsAsync(IEnumerable<CreateProductCommand> createRequests)
         {
             var createResponses = new List<IObjectClientResponse<CreateProductCommandResponse>>();
@@ -51,14 +67,163 @@ namespace Optivem.Template.Web.RestApi.IntegrationTest.Fixtures
             return createResponses;
         }
 
-        protected Task<IObjectClientResponse<CreateCustomerCommandResponse>> CreateCustomerAsync(CreateCustomerCommand createRequest)
+        protected async Task<IObjectClientResponse<CreateCustomerCommandResponse>> CreateCustomerAsync(CreateCustomerCommand command)
         {
-            return Fixture.Api.Customers.CreateCustomerAsync(createRequest);
+            var httpResponse = await Fixture.Api.Customers.CreateCustomerAsync(command);
+            httpResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+            return httpResponse;
         }
 
-        protected Task<IObjectClientResponse<CreateProductCommandResponse>> CreateProductAsync(CreateProductCommand createRequest)
+        protected async Task<IObjectClientResponse<CreateOrderCommandResponse>> CreateOrderAsync(CreateOrderCommand command)
         {
-            return Fixture.Api.Products.CreateProductAsync(createRequest);
+            var httpResponse = await Fixture.Api.Orders.CreateOrderAsync(command);
+            httpResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+            return httpResponse;
         }
+
+        protected async Task<IObjectClientResponse<CreateProductCommandResponse>> CreateProductAsync(CreateProductCommand command)
+        {
+            var httpResponse = await Fixture.Api.Products.CreateProductAsync(command);
+            httpResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+            return httpResponse;
+        }
+
+        protected Task<List<IObjectClientResponse<CreateCustomerCommandResponse>>> CreateSampleCustomersAsync()
+        {
+            var createRequests = new List<CreateCustomerCommand>
+            {
+                new CreateCustomerCommand
+                {
+                    FirstName = "Mary2",
+                    LastName = "Smith2",
+                },
+
+                new CreateCustomerCommand
+                {
+                    FirstName = "John2",
+                    LastName = "McDonald2",
+                },
+
+                new CreateCustomerCommand
+                {
+                    FirstName = "Jake2",
+                    LastName = "McDonald2",
+                }
+            };
+
+            return CreateCustomersAsync(createRequests);
+        }
+
+        protected async Task<List<IObjectClientResponse<CreateOrderCommandResponse>>> CreateSampleOrdersAsync()
+        {
+            var createCustomerResponses = await CreateSampleCustomersAsync();
+            var createProductResponses = await CreateSampleProductsAsync();
+
+            return await CreateSampleOrdersAsync(createCustomerResponses, createProductResponses);
+        }
+
+        protected Task<List<IObjectClientResponse<CreateOrderCommandResponse>>> CreateSampleOrdersAsync(List<IObjectClientResponse<CreateCustomerCommandResponse>> createCustomerResponses,
+            List<IObjectClientResponse<CreateProductCommandResponse>> createProductResponses)
+        {
+            var customerId_0 = createCustomerResponses[0].Data.Id;
+            var customerId_1 = createCustomerResponses[1].Data.Id;
+
+            var productId_0 = createProductResponses[0].Data.Id;
+            var productId_1 = createProductResponses[1].Data.Id;
+            var productId_2 = createProductResponses[2].Data.Id;
+
+            var createRequests = new List<CreateOrderCommand>
+            {
+                new CreateOrderCommand
+                {
+                    CustomerId = customerId_0,
+
+                    OrderItems = new List<CreateOrderItemCommand>
+                    {
+                        new CreateOrderItemCommand
+                        {
+                            ProductId = productId_0,
+                            Quantity = 30,
+                        },
+
+                        new CreateOrderItemCommand
+                        {
+                            ProductId = productId_1,
+                            Quantity = 60,
+                        },
+                    },
+                },
+
+                new CreateOrderCommand
+                {
+                    CustomerId = customerId_1,
+
+                    OrderItems = new List<CreateOrderItemCommand>
+                    {
+                        new CreateOrderItemCommand
+                        {
+                            ProductId = productId_1,
+                            Quantity = 40,
+                        },
+
+                        new CreateOrderItemCommand
+                        {
+                            ProductId = productId_2,
+                            Quantity = 50,
+                        },
+                    },
+                },
+            };
+
+            return CreateOrdersAsync(createRequests);
+        }
+
+        protected Task<List<IObjectClientResponse<CreateProductCommandResponse>>> CreateSampleProductsAsync()
+        {
+            var createRequests = new List<CreateProductCommand>
+            {
+                new CreateProductCommand
+                {
+                    Code = "APP2",
+                    Description = "Apple2",
+                    UnitPrice = 102.50m,
+                },
+
+                new CreateProductCommand
+                {
+                    Code = "BAN2",
+                    Description = "Banana2",
+                    UnitPrice = 302.99m,
+                },
+
+                new CreateProductCommand
+                {
+                    Code = "ORG2",
+                    Description = "Orange2",
+                    UnitPrice = 102.50m,
+                },
+
+                new CreateProductCommand
+                {
+                    Code = "MAN2",
+                    Description = "Mango2",
+                    UnitPrice = 500.99m,
+                },
+            };
+
+            return CreateProductsAsync(createRequests);
+        }
+
+
+        /*
+         * 
+
+
+
+
+
+         * 
+         */
+
     }
 }
