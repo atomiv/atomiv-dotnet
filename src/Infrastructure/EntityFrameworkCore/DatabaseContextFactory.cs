6572b8f1fbe86af37f38ce6cd61ccead357987c1
Valentina Cupać <valentina.cupac@optivem.com>
2020-06-30 15:08:45 +0200

    Template - Testing - Multiple environments #546

---
+++ src/Infrastructure/EntityFrameworkCore/DatabaseContextFactory.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atomiv.Infrastructure.EntityFrameworkCore
+{
+    public abstract class DatabaseContextFactory<TDbContext> : IDesignTimeDbContextFactory<TDbContext>
+        where TDbContext : DbContext
+    {
+        private const string DefaultEnvironment = "Development";
+
+        public TDbContext CreateDbContext(string[] args)
+        {
+            var environment = GetEnvironment();
+
+            Console.WriteLine($"The environment is {environment}.");
+
+            var configurationBuilder = CreateConfigurationBuilder(environment);
+            var configuration = configurationBuilder.Build();
+
+            var connectionString = GetConnectionString(configuration);
+
+            var optionsBuilder = new DbContextOptionsBuilder<TDbContext>();
+
+            ConfigureDbContextOptionsBuilder(optionsBuilder, connectionString);
+
+            var options = optionsBuilder.Options;
+
+            return CreateDbContext(options);
+        }
+
+        private string GetEnvironment()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (environment == null)
+            {
+                environment = DefaultEnvironment;
+            }
+
+            return environment;
+        }
+
+
+
+        protected abstract TDbContext CreateDbContext(DbContextOptions<TDbContext> options);
+
+        protected abstract IConfigurationBuilder CreateConfigurationBuilder(string environment);
+
+        protected abstract string GetConnectionString(IConfigurationRoot configuration);
+
+        protected virtual void ConfigureDbContextOptionsBuilder(DbContextOptionsBuilder<TDbContext> optionsBuilder, string connectionString)
+        {
+            // NOTE: No code here, can be specified in subclasses
+        }
+    }
+}
--- template/microservice/src/Tools/Atomiv.Template.Tools.Migrator/DatabaseContextFactory.cs
+++ template/microservice/src/Tools/Atomiv.Template.Tools.Migrator/DatabaseContextFactory.cs
@@ -3,60 +3,38 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Atomiv.Template.Infrastructure.Domain.Persistence.Common;
 using System;
+using Atomiv.Infrastructure.EntityFrameworkCore;
 
 namespace Atomiv.Template.Tools.Migrator
 {
-    public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    public class DatabaseContextFactory : DatabaseContextFactory<DatabaseContext>
     {
-        private const string DefaultEnvironment = "Development";
         private const string ConnectionKey = "DefaultConnection";
         private const string MigrationsAssembly = "Atomiv.Template.Tools.Migrator";
 
-        public DatabaseContext CreateDbContext(string[] args)
+
+
+        protected override IConfigurationBuilder CreateConfigurationBuilder(string environment)
         {
-            var environment = GetEnvironment();
-
-            Console.WriteLine($"The environment is {environment}.");
-
-            var connectionString = GetConnectionString(environment);
-
-            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
-            optionsBuilder.UseSqlServer(connectionString,
-                options => options.MigrationsAssembly(MigrationsAssembly));
-
-            return new DatabaseContext(optionsBuilder.Options);
-        }
-
-        private string GetEnvironment()
-        {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            if (environment == null)
-            {
-                environment = DefaultEnvironment;
-            }
-
-            return environment;
-        }
-
-        private string GetConnectionString(string environment)
-        {
-            var configuration = GetConfigurationRoot(environment);
-
-            var connection = configuration.GetConnectionString(ConnectionKey);
-
-            return connection;
-        }
-
-        private IConfigurationRoot GetConfigurationRoot(string environment)
-        {
-            var configurationBuilder = new ConfigurationBuilder()
+            return new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", false, true)
                 .AddJsonFile($"appsettings.{environment}.json", true);
+        }
 
-            var configuration = configurationBuilder.Build();
+        protected override string GetConnectionString(IConfigurationRoot configuration)
+        {
+            return configuration.GetConnectionString(ConnectionKey);
+        }
 
-            return configuration;
+        protected override void ConfigureDbContextOptionsBuilder(DbContextOptionsBuilder<DatabaseContext> optionsBuilder, string connectionString)
+        {
+            optionsBuilder.UseSqlServer(connectionString,
+                options => options.MigrationsAssembly(MigrationsAssembly));
+        }
+
+        protected override DatabaseContext CreateDbContext(DbContextOptions<DatabaseContext> options)
+        {
+            return new DatabaseContext(options);
         }
     }
 }
