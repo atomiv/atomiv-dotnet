328ce34caadc887d3d6f00c944ed21d760b64bac
Valentina Cupać <valentina.cupac@optivem.com>
2020-06-30 14:42:38 +0200

    Template - Testing - Multiple environments #546

--- template/microservice/src/Tools/Atomiv.Template.Tools.Migrator/DatabaseContextFactory.cs
+++ template/microservice/src/Tools/Atomiv.Template.Tools.Migrator/DatabaseContextFactory.cs
@@ -2,39 +2,61 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Atomiv.Template.Infrastructure.Domain.Persistence.Common;
+using System;
 
 namespace Atomiv.Template.Tools.Migrator
 {
     public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
     {
+        private const string DefaultEnvironment = "Development";
+        private const string ConnectionKey = "DefaultConnection";
+        private const string MigrationsAssembly = "Atomiv.Template.Tools.Migrator";
+
         public DatabaseContext CreateDbContext(string[] args)
         {
-            // TODO: VC: Handling multiple environments
+            var environment = GetEnvironment();
 
-            // var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine($"The environment is {environment}.");
 
-            var environment = "Development";
-            // throw new Exception("Environment " + environment);
+            var connectionString = GetConnectionString(environment);
 
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            optionsBuilder.UseSqlServer(connectionString,
+                options => options.MigrationsAssembly(MigrationsAssembly));
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+
+        private string GetEnvironment()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (environment == null)
+            {
+                environment = DefaultEnvironment;
+            }
+
+            return environment;
+        }
+
+        private string GetConnectionString(string environment)
+        {
+            var configuration = GetConfigurationRoot(environment);
+
+            var connection = configuration.GetConnectionString(ConnectionKey);
+
+            return connection;
+        }
+
+        private IConfigurationRoot GetConfigurationRoot(string environment)
+        {
             var configurationBuilder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", false, true)
                 .AddJsonFile($"appsettings.{environment}.json", true);
 
             var configuration = configurationBuilder.Build();
 
-
-            // TODO: VC: Check if dependency is allowed
-            var connectionKey = "DefaultConnection"; // TODO: VC:
-            var connection = configuration.GetConnectionString(connectionKey);
-
-            // TODO: VC: Perhaps make some external migrator console app?
-
-            // throw new Exception("Connection is: " + connection);
-
-            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
-            optionsBuilder.UseSqlServer(connection, options => options.MigrationsAssembly("Atomiv.Template.Tools.Migrator"));
-
-            return new DatabaseContext(optionsBuilder.Options);
+            return configuration;
         }
     }
 }
