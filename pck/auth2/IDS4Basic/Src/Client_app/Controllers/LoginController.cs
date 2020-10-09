c4ef1bcc6079dcd0998a1de306d8b7abe12698c4
Jelena Cupac <jelena.cupac@optivem.com>
2020-10-09 22:03:32 +0200

    Identity Server 4 - InMemory (Client app - Login page)

---
+++ pck/auth2/IDS4Basic/Src/Client_app/Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client_app.Controllers
+{
+	public class LoginController : Controller
+	{
+		// This login screen should be in Identity Server, just redirect to Identity Server
+		// taht will take it to show the login screen
+
+
+		// GET: LoginController
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		//public ActionResult SignIn()
+		//{
+		//	// Authentication
+		//}
+
+		//// GET: LoginController/Details/5
+		//public ActionResult Details(int id)
+		//{
+		//	return View();
+		//}
+
+		//// GET: LoginController/Create
+		//public ActionResult Create()
+		//{
+		//	return View();
+		//}
+
+		//// POST: LoginController/Create
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public ActionResult Create(IFormCollection collection)
+		//{
+		//	try
+		//	{
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	catch
+		//	{
+		//		return View();
+		//	}
+		//}
+
+		//// GET: LoginController/Edit/5
+		//public ActionResult Edit(int id)
+		//{
+		//	return View();
+		//}
+
+		//// POST: LoginController/Edit/5
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public ActionResult Edit(int id, IFormCollection collection)
+		//{
+		//	try
+		//	{
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	catch
+		//	{
+		//		return View();
+		//	}
+		//}
+
+		//// GET: LoginController/Delete/5
+		//public ActionResult Delete(int id)
+		//{
+		//	return View();
+		//}
+
+		//// POST: LoginController/Delete/5
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public ActionResult Delete(int id, IFormCollection collection)
+		//{
+		//	try
+		//	{
+		//		return RedirectToAction(nameof(Index));
+		//	}
+		//	catch
+		//	{
+		//		return View();
+		//	}
+		//}
+	}
+}
