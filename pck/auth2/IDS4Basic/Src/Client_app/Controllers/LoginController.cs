1582dcd638076b2b2d6f44f728b287ec1d060b9a
Jelena Cupac <jelena.cupac@optivem.com>
2020-10-09 23:30:04 +0200

    Identity Server 4 - InMemory (Client app - Integrate with Identity Server)

--- pck/auth2/IDS4Basic/Src/Client_app/Controllers/LoginController.cs
+++ pck/auth2/IDS4Basic/Src/Client_app/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,12 @@ namespace Client_app.Controllers
 		// GET: LoginController
 		public ActionResult Login()
 		{
-			return View();
+			//return View();
+			// instead of local view
+			// Challenge is a Microsoft MVC function
+			// Home controller, Index Action
+			return Challenge(new AuthenticationProperties() { RedirectUri = "Home/Index" },
+				Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme);
 		}
 
 		//public ActionResult SignIn()
--- pck/auth2/IDS4Basic/Src/Client_app/Startup.cs
+++ pck/auth2/IDS4Basic/Src/Client_app/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Client_app
 {
@@ -23,6 +26,45 @@ namespace Client_app
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+
+			// configure client app, integrate with identity server
+			// Client - Configure Identity Service
+			// write a separate method
+			ConfigureIdentityServer(services);
+		}
+		//created this method
+		private void ConfigureIdentityServer(IServiceCollection services)
+		{
+			var builder = services.AddAuthentication(options => SetAuthenticationOptions(options));
+
+			builder.AddCookie();
+			builder.AddOpenIdConnect(options => SetOpenIdConnectOptions(options));
+		}
+
+		private void SetOpenIdConnectOptions(OpenIdConnectOptions options)
+		{
+			// IDS4.InMem > Properties
+			options.Authority = "http://localhost:5001";
+			// match the client's id name in the configuration
+			options.ClientId = "client_app";
+			options.RequireHttpsMetadata = false;
+			options.Scope.Add("profile");
+			options.Scope.Add("openid");
+			options.ResponseType = "id_token";
+		}
+
+
+		// created this method
+
+
+		// craeted this method
+		private void SetAuthenticationOptions(AuthenticationOptions options)
+		{
+			// constant for this is "Cookies"
+			options.DefaultScheme = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme;
+			// constant for this is "OpenIdConnect"
+			options.DefaultChallengeScheme = Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectDefaults.AuthenticationScheme;
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +79,13 @@ namespace Client_app
 				app.UseExceptionHandler("/Home/Error");
 			}
 			app.UseStaticFiles();
+			// added
+			app.UseCookiePolicy();
 
 			app.UseRouting();
 
+			// Identity Server configuration client side
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
