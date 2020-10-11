7830047577136ab9b3b3c79e37c684c6ea6aa5e6
Jelena Cupac <jelena.cupac@optivem.com>
2020-10-11 13:21:43 +0200

    IdentityServer - IS4Demo - is4aspid - SQL Server notes (not implemented)

--- pck/auth2/IdentityServer/IS4Demo/Startup.cs
+++ pck/auth2/IdentityServer/IS4Demo/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.JSInterop.Infrastructure;
+using System.Reflection;
 
 namespace IS4Demo
 {
@@ -30,7 +32,14 @@ namespace IS4Demo
         {
             services.AddControllersWithViews();
 
+            //... Store conenction string as a var
+            //... var connectionString = Configuration.GetConnectionString("DefaultConnection")
+            //... Store assembly for migrations
+            //... var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            // Replace DbContext database from SqLite in template to SQL Server
             services.AddDbContext<ApplicationDbContext>(options =>
+                //... options.UseSqlServer(connectionString));
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
@@ -39,17 +48,29 @@ namespace IS4Demo
 
             var builder = services.AddIdentityServer(options =>
             {
-                options.Events.RaiseErrorEvents = true;
-                options.Events.RaiseInformationEvents = true;
-                options.Events.RaiseFailureEvents = true;
-                options.Events.RaiseSuccessEvents = true;
+                //options.Events.RaiseErrorEvents = true;
+                //options.Events.RaiseInformationEvents = true;
+                //options.Events.RaiseFailureEvents = true;
+                //options.Events.RaiseSuccessEvents = true;
 
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
+                //// see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                //options.EmitStaticAudienceClaim = true;
             })
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
+                //... use our SQL db for storing configuration data
+                /*... instead of 3 lines above
+                .AddConfigurationStore(configDb =>
+				{
+                    configDb.ConfigureDbContext = db => db.UseSqlServer(connectionString,
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+				})
+                //...use our SQL Database for storing operational data
+                .AddOperationalStore(operationalDb => {
+                    operationDb.ConfigureDbContexr = db => db.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+                })*/
                 .AddAspNetIdentity<ApplicationUser>();
 
             // not recommended for production - you need to store your key material somewhere secure
@@ -63,13 +84,15 @@ namespace IS4Demo
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
                     // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
+                    options.ClientId = "656662697276-duaugu330rkl2ep94k7s7p23vddpukm8.apps.googleusercontent.com";
+                    options.ClientSecret = "vFeZzfkiX-_bE3njm8CrZ1g";
                 });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            //... InitializeDatabase(app);
+
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -85,6 +108,72 @@ namespace IS4Demo
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            /*
+             * private void InitializeDatabase(IApplicationBuilder app) {
+             *  // using a services scope
+                 * using (var service Scope =
+                 *      app.ApplicationServices.GetService<IserviceScopefactory>()
+                 *      .CreateScope())
+                 * {
+                 *      // create PersistedGrant Database (we're using a single db here)
+                 *      var persistedGrantDbContext = serviceScope.ServiceProvider
+                 *          .GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                 *      persistedGrantDbContext.Database.Migrate();
+                 *      
+                 *      var configDbContext = serviceScope.ServiceProvider
+                 *          .GetRequiredService<ConfigurationDbContext>();
+                 *      configDbContext.Database.Migrate();
+                 *      
+                 *      if(!configDbContext.Clients.Any()) {
+                 *          foreach(var client in Config.GetClients()) {
+                 *              configDbContext.Clients.Add(client.ToEntity());
+                 *          }
+                 *          configDbContext.SaveChanges();
+                 *      }
+                 *      
+                 *      if(!configDbContext.IdentityResources.Any()) {
+                 *          foreach(var res in Config.GetIdentityResources()) {
+                 *              configDbContext.IdentityResources.Add(res.ToEntity());
+                 *          }
+                 *          configDbContext.SaveChanges();
+                 *      }
+                 *      
+                 *      if(!configDbContext.ApiResources.Any()) {
+                 *          foreach(var api in Config.GetApis()) {
+                 *              configDbContext.ApiResources.Add(api.ToEntity());
+                 *          }
+                 *          configDbContext.SaveChanges();
+                 *      }
+                 *      
+                 *  }
+             *  }
+             * */
         }
     }
 }
+
+
+// NOTE: 
+//... appsettings.json -> edit "DefaultConnection" for the SQL Server
+//... open up SQL Server
+//... In SQL create database etc..
+//... delete Migrations folder from Data folder
+
+//... for migrations, check if it's installed
+//... ISD4Demo> dotnet ef migrations add initial_identity_migration -c ApplicationDbContext -o data/Migrations/AspNetIdentity/ApplicationDb
+//... ISD4Demo> dotnet ef migrations add initial_is4_server_config_migration -c ConfigurationDbContext -o data/Migrations/AspNetIdentity/ConfigurationDb
+//... he created a database, gave permission
+//... ISD4Demo> dotnet ef migrations add initial_is4_persisted_grant_migration -c PersistedGrantDbContext -o data/Migrations/AspNetIdentity/PersistedGrantDb
+//... ISD4Demo> dotnet ef database update --context ApplicationDbContext
+//... go into SQL Server and you will see all the tabels like AspNetUsers
+//... generate tables that identity server 4 requires
+//... ISD4Demo> dotnet ef database update --context ConfigurationDbContext
+//... 
+//... ISD4Demo> dotnet ef database update --context PersistedGrantDbContext
+
+//... AspNetUsers (SQL Server) , ClientClaims, identityResources (IDS4), PersistedGrants (PersistedGrants)
+
+// seed our database with initial data, note SeedData.cs
+// ISD4Demo> dotnet run /seed
+// users were created with claims, added to AspNetUsers table in SQL Server...
