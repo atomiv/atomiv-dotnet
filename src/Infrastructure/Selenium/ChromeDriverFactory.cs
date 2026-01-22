e0939f972c57523dcd0943aa8c18ea418c379a1d
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-01-22 20:41:41 +0100

    Add headless Chrome options for CI environment
    - Detect CI environment via CI environment variable
    - Enable headless mode with --headless=new flag
    - Add --no-sandbox and --disable-dev-shm-usage for CI compatibility
    - Fixes Chrome instance exit errors in GitHub Actions

--- src/Infrastructure/Selenium/ChromeDriverFactory.cs
+++ src/Infrastructure/Selenium/ChromeDriverFactory.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 using Atomiv.Core.Common;
+using System;
 using System.IO;
 using System.Reflection;
 
@@ -10,7 +11,23 @@ namespace Atomiv.Infrastructure.Selenium
         public Driver Create()
         {
             var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var webDriver = new ChromeDriver(currentDir);
+            
+            var options = new ChromeOptions();
+            
+            // Check if running in CI environment
+            var isCi = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CI"));
+            
+            if (isCi)
+            {
+                // Options for headless CI environment
+                options.AddArgument("--headless=new");
+                options.AddArgument("--no-sandbox");
+                options.AddArgument("--disable-dev-shm-usage");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--window-size=1920,1080");
+            }
+            
+            var webDriver = new ChromeDriver(currentDir, options);
             var driver = new Driver(webDriver);
             return driver;
         }
